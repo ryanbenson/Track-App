f1e31a41a8e1379126b675e91212a4c48dba6566
Ryan Benson <benson.ryan@gmail.com>
2019-03-04 16:59:29 -0600

    Adds achievement list

---
+++ src/components/Achievement.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="achievement">
+    <h3>{{title}}</h3>
+    <p>{{due}}</p>
+    <p>{{week}}</p>
+    <p>{{year}}</p>
+    <p>{{level}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Achievement",
+  props: {
+    title: String,
+    due: Date,
+    week: String,
+    year: String,
+    level: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Achievements.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="achievements">
+    <div v-if="!hasAchievements" class="achievements--none">
+      <p>No achievements found</p>
+    </div>
+    <div v-else class="achievements--list">
+      <Achievement
+        v-for="(achievement, index) in achievements"
+        class="achievements--item"
+        :key="achievement.index"
+        :title="achievement.title"
+        :due="achievement.due"
+        :week="achievement.week"
+        :year="achievement.year"
+        :level="achievement.level"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import Achievement from "./Achievement.vue";
+export default {
+  name: "Achievements",
+  components: {
+    Achievement
+  },
+  data() {
+    return {
+      achievements: []
+    };
+  },
+  computed: {
+    hasAchievements() {
+      if (this.achievements && this.achievements.length > 0) return true;
+      return false;
+    }
+  },
+  async mounted() {
+    const url = `${process.env.VUE_APP_API_URL}/achievements`;
+    let response = await fetch(url);
+    let data = await response.json();
+    this.achievements = data;
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
